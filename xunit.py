e1584431309c789cf9a2c95c05af4a373e99e7d1
Elvis Attro <attroelvis@gmail.com>
2019-02-25 23:20:10 +0000

    Make the previous failling test, compile and pass.

--- xunit.py
+++ xunit.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class TestCase:
+    def __init__(self):
+        self.wasRun = False
 
-test = Testcase()
+    def testMethod(self):
+        self.wasRun = True
+
+test = TestCase()
 print(test.wasRun)
 test.testMethod()
 print(test.wasRun)
