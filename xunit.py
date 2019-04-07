4f9c72a0da9e5beae954a761efbc8b2dbbc5dacd
Elvis Attro <attroelvis@gmail.com>
2019-04-07 14:57:49 +0000

    Counting/Collecting Failed results.
    When does a test execution fail? Mostly in 2 cases: Either there is an
    execution error during setup() or run(), either the method under test
    runs properly but the assertion associated with it is not satisfied.
    In both cases, an exception is thrown, but i feel like i have to deal
    with these 2 separately.
    Fpr now, let focus on when the assertion is not correct. In this case,
    we catch the assertion, consider this as test failure and increment the
    failures count. As the assertion is caught, we can no longer rely on it,
    instead, we need to directly print/report the test results on the console.

--- xunit.py
+++ xunit.py
@@ -24,18 +24,30 @@ class TestCase:
         results = TestResults()
         self.setUpTemplate()
         method = getattr(self, self.methodName)
-        method()
+        try:
+            method()
+        except(AssertionError):
+            results.failCount += 1
         results.testCount += 1
         self.log = self.log + "-Running()"
         self.tearDownTemplate()
         return results
 
+    def reportResults(self):
+        results = self.run()
+        print(results.resultsSummary())
+
+
 class TestResults():
     def __init__(self):
         self.testCount = 0
+        self.failCount = 0
 
     def resultsSummary(self):
-        return "OK. {} Run, 0 Failled.".format(self.testCount)
+        status = "FAILURE" if self.failCount > 0 else "OK"
+        return "{}. {} Run, {} Failled.".format(
+            status, self.testCount, self.failCount)
+
 
 class WasRun(TestCase):
     def setUp(self):
@@ -53,10 +65,15 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert(self.test.log == "Setup()-Running()-TearDown()")
 
-    def testResults(self):
+    def testPassingResults(self):
         results = self.test.run()
         assert("OK. 1 Run, 0 Failled." == results.resultsSummary())
 
+    def testFaillingResults(self):
+        results = self.test.run()
+        assert("FAILURE. 1 Run, 1 Failled." == results.resultsSummary())
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testResults").run()
+
+TestCaseTest("testRunning").reportResults()
+TestCaseTest("testPassingResults").reportResults()
+TestCaseTest("testFaillingResults").reportResults()
