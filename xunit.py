893d784270a9a2f9c6586b228ec16842b0f6ca55
Elvis Attro <attroelvis@gmail.com>
2019-04-07 14:17:24 +0000

    Counting test executions.
    The implementation of TearDown even after an exception is critical
    because it implies catching exceptions. Once we start catching
    exceptions, we can no longer assess or know to what causes attribute any
    success/failure of our test execution. Thus, relying on exceptions to
    report test failures, as we did until now is no longer viable. We need a
    new way, such as collecting/counting results while catching exceptions and
    probably report them on a log or console line. Therefore, we add a new test
    in our test list which is "Counting test results".
    From now, we can collect/count tests executions.

--- xunit.py
+++ xunit.py
@@ -21,12 +21,21 @@ class TestCase:
         pass
 
     def run(self):
+        results = TestResults()
         self.setUpTemplate()
         method = getattr(self, self.methodName)
         method()
+        results.testCount += 1
         self.log = self.log + "-Running()"
         self.tearDownTemplate()
+        return results
 
+class TestResults():
+    def __init__(self):
+        self.testCount = 0
+
+    def resultsSummary(self):
+        return "OK. {} Run, 0 Failled.".format(self.testCount)
 
 class WasRun(TestCase):
     def setUp(self):
@@ -44,5 +53,10 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert(self.test.log == "Setup()-Running()-TearDown()")
 
+    def testResults(self):
+        results = self.test.run()
+        assert("OK. 1 Run, 0 Failled." == results.resultsSummary())
+
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testResults").run()
